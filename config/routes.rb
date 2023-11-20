2696d1a85fdcbad24f96c9b6ef029610b029a0bd
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-20 11:43:37 +0100

    User story: edit task

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   post "tasks", to: "tasks#create"
   get "tasks/:id", to: "tasks#show", as: :task
 
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch  "tasks/:id",      to: "tasks#update"
+
   # Defines the root path route ("/")
   root "tasks#index"
 end
