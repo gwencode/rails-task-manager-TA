bca022c27d301a1f4b835bd502da12cec210ccc4
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-20 12:33:01 +0100

    User story: delete task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -25,6 +25,10 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
 
   private
 
